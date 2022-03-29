5312deeb4d9c70aa1ea63ee8913a47121e6de818
Matheus Vianei de Freitas <mvdfreitas@gmail.com>
2022-03-29 12:06:01 -0300

    Migration da tabela de Clientes

---
+++ database/migrations/2022_03_29_135123_create_clientes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',100);
+            $table->date('nascimento');
+            $table->string('cpf',11);
+            $table->char('sexo',1);
+            $table->string('telefone',15);
+
+            $table->string('email', 100);
+            $table->string('senha', 50);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
