983c9bc7d4fada5b1295bf6f90e747abb82bf575
Matheus Freitas <mvdfreitas@gmail.com>
2022-06-21 15:30:02 -0300

    Slug - função alterar para substituir letra com acento, pois a anterior não estava fazendo

--- app/Helpers/Slug.php
+++ app/Helpers/Slug.php
@@ -6,10 +6,28 @@ class Slug
 {
     public static function makeSlug($value)
     {
-        $slug = trim($value); // trim the string
-        $slug = preg_replace('/[^a-zA-Z0-9 -]/','',$slug ); // only take alphanumerical characters, but keep the spaces and dashes too...
-        $slug = str_replace(' ','-', $slug); // replace spaces by dashes
-        $slug = strtolower($slug);  // make it lowercase
-        return $slug;
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\pL\d]+~u', '-', $value);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // remove duplicated - symbols
+        $text = preg_replace('~-+~', '-', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
     }
 }
